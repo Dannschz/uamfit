13ebc54ce56983fedd2f396764856395952dd3bb
Daniel <daniel.snchz.develop@outlook.com>
2020-06-10 22:37:41 -0500

    Adding p tag

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-          UAM-FIT
-        </h1>
+        <h1>UAM-FIT</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+          assumenda numquam in quae facere, eos hic tenetur suscipit accusantium
+          maxime mollitia consequatur sunt temporibus perferendis minus
+          voluptatum dolore quod nulla?
+        </p>
       </header>
     </div>
   );
